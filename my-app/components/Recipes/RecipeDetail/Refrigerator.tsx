ecba53a5510077508603ca8f2d91025b383061bf
manatomato <manato2002manato@gmail.com>
2025-09-10 20:56:45 +0900

    ナビゲーションバーに特定のパスで非表示にする条件を追加し、レシピ画像コンポーネントに戻るボタンの機能を実装しました。また、冷蔵庫コンポーネントのローカルストレージからのデータ取得処理を修正し、材料がない場合のメッセージを表示するようにしました。新たにバックボタンのカスタムフックを追加しました。

--- my-app/components/Recipes/RecipeDetail/Refrigerator.tsx
+++ my-app/components/Recipes/RecipeDetail/Refrigerator.tsx
@@ -11,18 +11,13 @@ export default function Refrigerator() {
     const [refrigeratorIngredients, setRefrigeratorIngredients] = useState<RefrigeratorIngredient[]>([]);
 
     useEffect(() => {
-        const storedIngredients = localStorage.getItem("refrigeratorIngredients");
+        const storedIngredients = localStorage.getItem("extracted_ingredients");
         if (storedIngredients) {
             setRefrigeratorIngredients(JSON.parse(storedIngredients));
         } else {
-            // Initialize with dummy data if not present
-            const dummyData = [
-                {id: 1, name: "鶏肉", amount: 200},
-                {id: 2, name:"卵", amount: 2},
-                {id: 3, name:"ゴーヤ", amount:1}
-            ];
-            localStorage.setItem("refrigeratorIngredients", JSON.stringify(dummyData));
-            setRefrigeratorIngredients(dummyData);
+            <div>
+                冷蔵庫の材料がありません。
+            </div>
         }
     }, []);
 
@@ -35,10 +30,10 @@ export default function Refrigerator() {
             <div className="text-zinc-800 text-xl font-bold font-['Noto_Sans_JP'] leading-tight">冷蔵庫の材料</div>
             <div className="self-stretch flex flex-col justify-start items-start gap-2">
                 <div className="self-stretch flex flex-col justify-start items-start">
-                    {refrigeratorIngredients.map((ingredient) => (
-                        <div key={ingredient.id} className="self-stretch py-2 border-b border-zinc-800 inline-flex justify-between items-center gap-2">
-                            <div className="text-zinc-800 text-sm font-normal font-['Noto_Sans_JP'] leading-tight">{ingredient.name}</div>
-                            <div className="text-zinc-800 text-sm font-normal font-['Noto_Sans_JP'] leading-tight">{ingredient.amount}</div>
+                    {refrigeratorIngredients.map((ingredient, index) => (
+                        <div key={ingredient.toString() + index} className="self-stretch py-2 border-b border-zinc-800 inline-flex justify-between items-center gap-2">
+                            <div className="text-zinc-800 text-sm font-normal font-['Noto_Sans_JP'] leading-tight">{ingredient.toString()}</div>
+                            {/* <div className="text-zinc-800 text-sm font-normal font-['Noto_Sans_JP'] leading-tight">{ingredient}</div> */}
                         </div>
                     ))}
                 </div>
